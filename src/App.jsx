97b77413c42675d0f7b4f2aa10d347d8dca1a496
mousamvishwakarma <mousamvishwakarma929@gmail.in>
2025-03-16 00:08:28 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./pages/home";
+import Login from "./pages/login";
 function Body() {
   return (
     <div className="font-sans">
      <Home/>
+     <Login/>
     </div>
   );
 }
---
+++ src/pages/login.jsx
@@ -0,0 +1,17 @@
+import NewArrivals from "../components/layout/newarrival";
+import Hero from "../components/layout/hero";
+import Navbar from "../components/layout/Navbar";
+import StaticImageShowcase from "../components/layout/StaticImageShowcase";
+ 
+function Login() {
+  return (
+    <div>
+       <Navbar/>
+      <Hero />
+      <NewArrivals />  
+      <StaticImageShowcase/>
+    </div>
+  );
+}
+
+export default Login;  
